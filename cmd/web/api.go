4d4a684ccf2aeee084126ab897a7ffa2d988c809
Bryan McKelvey <bryan.mckelvey@gmail.com>
2020-03-05 17:48:11 +0900

    Try and get token using metadata server

--- cmd/web/api.go
+++ cmd/web/api.go
@@ -5,8 +5,8 @@ import (
 	"crypto/x509"
 	"fmt"
 	"os"
-	"strings"
 
+	"cloud.google.com/go/compute/metadata"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -30,28 +30,35 @@ func (tokenAuth) RequireTransportSecurity() bool {
 	return true
 }
 
+func isOnCloudRun() bool {
+	return os.Getenv("K_SERVICE") != ""
+}
+
 func getGrpcClientConnection(addr string) (*grpc.ClientConn, error) {
-	if strings.Contains(addr, "localhost") {
-		conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	pool, _ := x509.SystemCertPool()
+	ce := credentials.NewClientTLSFromCert(pool, "")
+
+	var token string
+	var err error
+	if isOnCloudRun() {
+		tokenUrl := fmt.Sprintf("/instance/service-accounts/default/identity?audience=%s", addr)
+		token, err = metadata.Get(tokenUrl)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("metadata.Get: failed to query id_token: %+v", err)
 		}
-		return conn, nil
 	} else {
-
-		pool, _ := x509.SystemCertPool()
-		ce := credentials.NewClientTLSFromCert(pool, "")
-
-		conn, err := grpc.Dial(
-			addr,
-			grpc.WithTransportCredentials(ce),
-			grpc.WithPerRPCCredentials(tokenAuth{token: os.Getenv("BRYMCK_ID_TOKEN")}),
-		)
-		if err != nil {
-			return nil, err
-		}
-		return conn, nil
+		token = os.Getenv("BRYMCK_ID_TOKEN")
 	}
+
+	conn, err := grpc.Dial(
+		addr,
+		grpc.WithTransportCredentials(ce),
+		grpc.WithPerRPCCredentials(tokenAuth{token: token}),
+	)
+	if err != nil {
+			  return nil, err
+			  }
+	return conn, nil
 }
 
 func getServiceAddress(serviceName string) string {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/brymck/risk-service
 go 1.13
 
 require (
+	cloud.google.com/go v0.53.0
 	github.com/brymck/helpers v0.0.0-20200305035254-abd9424de1a4
 	github.com/golang/protobuf v1.3.4
 	github.com/sirupsen/logrus v1.4.2
